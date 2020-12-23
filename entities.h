a329bc709b68f38abf4e2a80541fece67cfa859f
JohnKalesis1 <61728881+JohnKalesis1@users.noreply.github.com>
2020-12-23 22:34:19 +0200

    Update entities.h

--- entities.h
+++ entities.h
@@ -1,17 +1,10 @@
 #include <string>
 #include <list>
+#include <set>
 #include <vector>
 class Hero;
 class Monster;
 
-
-namespace {
-    std::string hero_names[]={"Abbathor","Aerdrie Faenya","Arvoreen","Baervan Wildwanderer","Baravar Cloakshadow","Berronar Truesilver","Brandobaris","Callarduran Smoothha",nds"Charmalaine","Clangeddin Silverbea",rd"Corellon Larethian","Cyrrollalee","Deep Sashelas","Dugmaren Brightmantl",e"Dumathoin","Ehlonna","Erevan Ilesere","Fenmarel Mestarine","Fharlanghn","Flandal Steelskin","Gadhelyn","Gaerdal Ironhand","Garl Glittergold","Gendwar Argrim","Halani Celanil","Heironeous","Labelas Enoreth","Moradin","Muamman Duathal","Olidammara","Pelor","Rillifane Rallathil","Roykyn","Segojan Earthcaller","Sehanine Moonbow","Sheela Peryroyl","Solonor Thelandira","St. Cuthbert","Urdlen","Urogalan","Vergadain","Ye'Cind","Yondalla","Annam","Bahamut","Blibdoolpoolp","Diirinka","Eadro","Gruumsh","Hruggek","Ilsensine","Jubilex","Kurtulmak","Laduguer","Loagzed","Lolth","Maglubiyet","Merrshaulk","Orcus","Sekolah","Semuanya","Shekinester","Skerrit","Stronmaus","Tiamat","Vaprak","Yeenoghu","Aasterinian","Bagrivyek","Bahgtru","Chronepsis","Doresain","Falazure","Gaknulak","Grankhul","Great Mother","Grolantor","Hiatea","Iallanis","Ilneval","Io","Karontor","Kiaransalee","Khurgorbaeyag","Luthic","Memnor","Nomog-Geaya","Panzuriel","Raxivort","Shargaas","Sixin","Skoraeus Stonebones","Squerrik","Surtr","Thrym","Vhaeraun","Yurtrus","Zinzerena"};
-    std::string potion_names[]={"Potion","Hi-Potion","X-Potion","Mega-Potion","Ether","Turbo Ether","Elixir","Megalixir","Phoenix Down","Mega Phoenix","Antidote","Echo Screen","Eye Drops","Holy Water","Remedy","Soft","Al Bhed Potion","Healing Water","Tetra Elemental","Candle of Life","Dream Powder","Farplane Shadow","Farplane Wind","Frag Grenade","Silence Grenade","Sleeping Powder","Smoke Bomb","Petrify Grenade","Poison Fang","Blessed Gem","Dark Matter","Shadow Gem","Shining Gem","Supreme Gem","Gold Hourglass","Healing Spring","Light Curtain","Lunar Curtain","Mana Spring","Mana Tablet","Mana Tonic","Purifying Salt","Silver Hourglass","Soul Spring","Stamina Spring","Stamina Tablet","Stamina Tonic","Star Curtain","Three Stars","Twin Stars","Ability Distiller","Amulet","Designer Wallet","Door to Tomorrow","Gambler's Spirit","Hypello Potion","Mana Distiller","Map","Musk","Pendulum","Power Distiller","Shining Thorn","Speed Distiller","Underdog's Secret","Wings to Discovery","Winning Formula"};
-    std::string spell_names[]={"Flame","Torch","Eruption","Flare","Blaze","Inferno","Fireball","Explosion","Meteor","Salamander’s Blade","Dragon Fire","Sinkhole","Pit","Chasm","Vine","Thorn","Nettle","Rockfall","Landslide","Avalanche","Chill","Freeze","Frostbite","Douse","Deluge","Flood","Wave","Surge","Tsunami","Nyad’s Lore","Sleep","Slumber","Shock","Bolt","Plasma","Gust","Gale","Tempest","Whirlwhind","Cyclone","Vortex","Wyvern’s Wings","Daze","Stun","Confuse","Light","Flash","Radiance","Cleanse","Scour","Purge","Wrath","Retribution","Judgment","Angel’s Mercy","Darkness","Shade","Gloom","Decay","Fester","Corrupt","Spite","Rancor","Malice","Demon’s Rage","Drain","Siphon","Void","Bolt Strike","Freeze Wave","Inferno Blaze","Channel Energy","Shadow Blade","Heaven's Sword","Blazing Katana","Staff of Ice","Arrow of Purity","Blazing Arrows","Burn the Soul","Fire Wings","Fury of Osano-Wo","Gaijin Flames","Hungry Blade","Yakamo’s Wrath","Yakamo’s Eye","Breath of Suitengu","The Inner Ocean","Surging Soul","Endless Depth","Kharmic Intent","Whispered Blade"};
-    std::string weapon_names[]={"Caladbolg","Taming Sword","Crystal Sword","Excalibur","Ragnarok","Balmung","Save the Queen","Heartbreaker","Lionheart","Durandal","Ascalon","Ambitious","Hrunting","Astral Sword","Apocalypse","Slowstrike","Master Sword","Rune-Master","Warlock","Arc Sword","Gilventure","Tri-Steel","Helter-Skelter","Vendetta","Avenger","Prism Steel","Mirage Sword","Lifesaver","Sonic Steel","Vigilante","Dance Macabre","Largamente","Gravestone","Sidewinder","Nightmare","Mage Masher","Nightbringer","Knight Sword","Wizard Sword","Double-Edge","Razzmatazz","Deathbringer","Stunning Steel","Basilisk Steel","Poison Steel","Lullaby Steel","Muted Steel","Twilight Steel","Hunter's Sword","Flametongue","Ice Brand","Lightning Steel","Force Saber","Baroque Sword","Sorcery Sword","Soldier's Saber","Rune Steel","Enchanted Sword","Fencing Saber","Slasher","Nirvana","Herding Staff","Arc Arcana","Abraxas","Heavenly Axis","Judgment","Seraphim Rod","Rod of Roses","Nimbus Rod","Wing Wand","Wonder Wand","Laevatein","Astral Rod","Chaos Rod","Power Staff","Shining Staff","Faerie Staff","Magistral Rod","El Dorado","Tri-Rod","Wicked Wand","Nemesis Rod","Defender","Prism Rod","Mirage Rod","Healing Rod","Wind Rod","Conductor","Punisher","Calcite Staff","Bizarre Staff","Staff of Thorns","Reticent Staff","Darkness Staff","Monk Staff","Mage's Staff","Dual Rod","Ominous Rod","Death Wand","Entangling Rod","Break Rod","Belladonna Wand","Lullaby Rod","Rod of Silence","Rod of Darkness","Rod of Wisdom","Rod of Fire","Rod of Ice","Rod of Lightning","Rod of Water","Malleable Staff","Force Rod","Ductile Rod","Sorcery Rod","Full Metal Rod","Rune Rod","Enchanted Rod","Rod of Striking","Spiked Rod"};
-    std::string armor_names[]={"Genji Shield","Emblem","Crystal Shield","Aegis Shield","Golem Shield","Revive Shield","Rescue Shield","Paladin Shield","Diamond Shield","Ruby Shield","Dynasty Shield","Magister Shield","Collector's Shield","Treasure Shield","Shield of Hope","Assault Shield","Phantom Shield","Recovery Shield","Spiritual Shield","Phoenix Shield","Curative Shield","Rainbow Shield","Shining Shield","Faerie Shield","Peaceful Shield","Shaman Shield","Barrier Shield","Star Shield","Marching Shield","Moon Shield","Regen Shield","Haste Shield","Reflect Shield","Protect Shield","Shell Shield","Platinum Shield","Sapphire Shield","Knight's Shield","Wizard Shield","Elemental Shield","Defending Shield","Crimson Shield","Snow Shield","Ochre Shield","Cerulean Shield","Medical Shield","Lucid Shield","Serene Shield","Light Shield","Soul Shield","Blessed Shield","Soft Shield","Serum Shield","Alert Shield","Echo Shield","Bright Shield","Red Shield","White Shield","Yellow Shield","Blue Shield","NulTide Shield","NulBlaze Shield","NulFrost Shield","Adept's Shield","Tetra Shield","Mythril Shield","Gold Shield","Emerald Shield","Soldier's Shield","Mage's Shield","Silver Shield","Onyx Shield","Sorcery Shield","Warrior's Shield","Glorious Shield","Metal Shield","Pearl Shield","Magic Shield","Seeker's Shield"};
-};
 
 
 class Effect  {
@@ -20,9 +13,9 @@ class Effect  {
         float percentage;
         int stat_affected;
     public:
-        Effect();
+        Effect() {}
         Effect(int duration,float percentage,int stat_affected) : duration(duration), percentage(percentage), stat_affected(stat_affected)  {}
-        ~Effect();
+        ~Effect() {}
         bool is_active();
         void reduce_duration();
         int apply_effect(int stat_value);
@@ -36,7 +29,6 @@ class Item  {
         int price;
         int lvl_requirement;
     public:
-        Item();
         Item(std::string name,int price,int lvl) : name(name), price(price), lvl_requirement(lvl) {}
         virtual ~Item() {}
         int get_lvl_requirement();
@@ -50,7 +42,7 @@ class Weapon: public Item  {
         bool Two_handed;
     public:
         Weapon(std::string name,int price,int lvl,int damage,bool two_handed) : Item(name,price,lvl), damage(damage), Two_handed(two_handed)  {}
-        Weapon() {}
+        ~Weapon() {}
         int attack();
         bool Two_handed_weapon();
 };
@@ -68,13 +60,14 @@ class Potion: public Item  {
     private:
         Effect effect;
     public:
-        Potion(std::string name,int price,int lvl,int duration,float percentage);
+        Potion(std::string name,int price,int lvl,int duration,float percentage,int stat_affected);
         ~Potion() {}
         void use(Hero* hero);
 };
 
 class Spell  {
     protected:
+        Effect effect;
         std::string name;
         int price;
         int lvl_requirement;
@@ -82,6 +75,7 @@ class Spell  {
         int avg_damage;
     public:
         Spell(std::string name,int price,int lvl,int mp_usage,int damage) : name(name), price(price), lvl_requirement(lvl), mp_usage(mp_usage), avg_damage(damage)  {}
+        virtual ~Spell() {}
         void initiate_spell(Monster* monster);
         int get_lvl_requirement();
         int get_price();
@@ -90,22 +84,16 @@ class Spell  {
 };
 
 class LigthingSpell: public Spell  {
-    protected: 
-        Effect effect;
     public:
         LigthingSpell(std::string name,int price,int lvl,int mp_usage,int damage);
 };
 
 class IceSpell: public Spell  {
-    protected: 
-        Effect effect;
     public:
         IceSpell(std::string name,int price,int lvl,int mp_usage,int damage);
 };
 
 class FireSpell: public Spell  {
-    protected: 
-        Effect effect;
     public:
         FireSpell(std::string name,int price,int lvl,int mp_usage,int damage);
 };
@@ -117,12 +105,11 @@ class LivingBeing  {
         int health;
         int level;
     public:
-        LivingBeing(std::string name,int health,int level) : name(name), health(health), level(level) {}
+        LivingBeing(std::string name,int health,int level) : name(name), health_capacity(health), health(health), level(level) {}
         ~LivingBeing() {}
         std::string get_name();
         int get_health();
         int get_level();
-        void die();
         void restore_life(int amount);
 };
 
@@ -135,34 +122,42 @@ class Hero: public LivingBeing  {
         std::vector<Effect> effects;// 0 slot is for strenght, 1 is for dexterity, and 2 is for agility
         int magic_power_capacity;
         int magic_power;
-        int strenght;
+        int base_strength;
+        int strength;
+        int base_dexterity;
         int dexterity;
+        int base_agility;
         int agility;
         int money;
         int experience;
     public:
-        Hero(std::string name,int magic_power_capacity,int strength,int dexterity,int agility,int money);
-        ~Hero();
+        Hero(std::string name,int health,int magic_power_capacity,int strength,int dexterity,int agility,int money)  : LivingBeing(name,health,1), magic_power_capacity(magic_power), magic_power(magic_power), strength(strength), base_strength(strength), base_dexterity(dexterity), dexterity(dexterity), base_agility(agility), agility(agility), money(money), armor(NULL), weapon(NULL)  {}        
+        virtual ~Hero();
         void display_stats();
-        void acces_inventory();
+        void access_inventory();
+        int get_magic_power();
+        int get_magic_power_capacity();
         int get_strenght();
         int get_dexterity();
         int get_agility();
         int get_wealth();
         int get_experience();
-        void cast_spell(int spell_selection,Monster* monster);
+        bool evade();
+        bool try_and_level_up();
+        void cast_spell(std::string name,Monster* monster);
+        void level_up();
         void attack(Monster* monster);
         void restore_mp(int amount);
         void increase_exp(int amount);
-        void incease_wealth(int amount);
-        void level_up();
+        void increase_wealth(int amount);
         void receive_damage(int damage);
-        void evade();
-        void lose_money();
-        void replace_weapon(int weapon_selection);
-        void replace_armor(int armor_selection);
+        void lose_money(int money);
+        void replace_weapon(std::string weapon_name);
+        void replace_armor(std::string armor_name);
         void unequip_armor();
+        void unequip_weapon();
         void use_potion(int potion_selection);
+        void receive_buff(Effect effect);
 };
 
 class Warrior: public Hero  {
@@ -195,7 +190,7 @@ class Monster: public LivingBeing  {
         void diplay_stats();
         void attack(Hero* hero);
         void evade();
-        void receive_damage();
+        void receive_damage(int damage);
         void receive_debuff(Effect& effect);
 };
 
